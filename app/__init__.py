00470a797976a24a09bac7f35dc91e289be3ad2b
Harrison <Boyns12345@gmail.com>
2021-04-09 15:06:21 +0100

    db changes made

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,21 @@ from app.resources.store import StoreList, Store
 from app.security.security import identity, authenticate
 from app.resources.user import UserRegister
 from app.resources.items import Items, ItemList
+from app.database.db import db
 
 # this __init__ file basically lets one import files
 app = Flask(__name__)
 
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 app.secret_key = "top_secret"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 jwt = JWT(app, authentication_handler=authenticate, identity_handler=identity)
 
